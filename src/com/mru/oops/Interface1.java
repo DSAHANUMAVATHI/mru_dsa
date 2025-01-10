21b2e45794a35e7fb3c1e25401469247babaa9f5
DSAHANUMAVATHI <hanumavathipothuganti06@gmail.com>
2025-01-10 14:29:17 +0530

    OOPS

---
+++ src/com/mru/oops/Interface1.java
@@ -0,0 +1,8 @@
+package com.mru.oops;
+
+interface Interface1{
+	public void addition();
+	public int subtraction(int num1,int num2);
+	public void multi(int num1,int num2);
+	public int division();
+}
