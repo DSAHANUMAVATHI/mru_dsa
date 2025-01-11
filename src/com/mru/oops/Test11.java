618321072734e2a9ba75b5c88baf7d7f9b655bcd
DSAHANUMAVATHI <hanumavathipothuganti06@gmail.com>
2025-01-11 10:22:26 +0530

    OOPS DAY-2

---
+++ src/com/mru/oops/Test11.java
@@ -0,0 +1,8 @@
+package com.mru.oops;
+
+public class Test11 {
+ String message = "Hanumavathi";
+ public void m1() {
+	 System.out.println("Hanumavathi");
+	 }
+}
---
+++ src/com/mru/oops/Test12.java
@@ -0,0 +1,9 @@
+package com.mru.oops;
+
+public class Test12 {
+	String msg = "Hanumavathi2";
+	public void m2() {
+		System.out.println("Hanumavathi2");
+	}
+
+}
